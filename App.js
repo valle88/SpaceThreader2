1c23c23d1f3e11e4576aa86b340450e769d3cf17
valle <vallukis@gmail.com>
2023-03-05 12:26:45 +0000

    feat: subir todo los cambios  en app

--- App.js
+++ App.js
@@ -4,19 +4,32 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 
 import HomeScreen from './screens/HomeScreen';
-import ProfileScreen from './screens/ProfileScreen';
-import spaceTraders from './screens/SpaceTraders';
+import LoanScreen from './screens/LoanScreen';
 import LoginScreen from './screens/LoginScreen';
-
+import ShipScreen from './screens/ShipScreen';
+import RegisterScreen from './screens/Login/RegisterScreen';
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
+    Object.entries(signIn).length === 0
+    ?
     <NavigationContainer>
       <Drawer.Navigator initialRouteName='Login'>
-        <Drawer.Screen name='LoginScreen' component={LoginScreen}/>
-        <Drawer.Screen name='Home' component={HomeScreen}/>
-        <Drawer.Screen name='Profile' component={ProfileScreen}/>
+        <Drawer.Screen name='Login' component={LoginScreen} options={{headerShown: false}} />
+        <Drawer.Screen name='SignIn' options={{headerShown: false}}>
+          {() => <SignInScreen setSignIn={setSignIn} />}
+        </Drawer.Screen>
+        <Drawer.Screen name='Register' component={RegisterScreen} options={{ headerShown: false }} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+    :
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName='Home'>
+        <Drawer.Screen name='Home' component={HomeScreen} />
+        <Drawer.Screen name='Loan' component={LoanScreen} />
+        <Drawer.Screen name='Ships' component={ShipScreen} />
+        <Drawer.Screen name='Login' component={LoginScreen} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
---
+++ screens/LoanScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoanScreen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LoanScreen
