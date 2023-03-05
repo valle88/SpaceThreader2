8d006782eee85b06501504de2661d16f88304249
valle <vallukis@gmail.com>
2023-03-05 11:35:25 +0000

    feat: configurar la programacion del singinScreen

--- screens/SiginScreen.jsx
+++ screens/SiginScreen.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import { View } from 'react-native'
 
-const SiginScreen = () => {
+const SiginScreen = ({setLoginIn}) => {
+
+  const [text,SetText] = useState('')
+
+  const Login = async () => {
+    try{
+      const userProfile = await spaceTraders.getUserProfile();
+      setSignIn(userProfile);
+
+      alert(`User: ${userProfile.user.username}\nwell authenticated`)
+
+    } catch(err) {
+      console.log(err.message);
+    }
+  }
   return (
     <View>
-      
+      <Text>please enter your token</Text>
+      <TextInput
+          style={styles.inputContainer}
+          onChangeText={onChangeText}
+          value={text} 
+      />
+      <Pressable style={styles.loginBtn} onPress={() => handleLogin()}><Text>Login</Text></Pressable>
+
     </View>
   )
 }
